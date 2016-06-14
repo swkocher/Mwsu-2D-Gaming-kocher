7eb594e1f6369fd6cde58cf8f2c9b9a6660a1f2f
Sam Kocher <sam.w.kocher@gmail.com>
2016-06-14 10:50:23 -0500

    Update play.js
    fixed skewed indentations

--- Program-2/js/play.js
+++ Program-2/js/play.js
@@ -7,27 +7,27 @@ var playState = {
         this.player.anchor.setTo(0.5, 0.5);
         game.physics.arcade.enable(this.player);
         this.player.body.gravity.y = 500;
-		// Create the 'right' animation by looping the frames 1 and 2
-		this.player.animations.add('right', [1, 2], 8, true);
-		// Create the 'left' animation by looping the frames 3 and 4
-		this.player.animations.add('left', [3, 4], 8, true);
+	// Create the 'right' animation by looping the frames 1 and 2
+	this.player.animations.add('right', [1, 2], 8, true);
+	// Create the 'left' animation by looping the frames 3 and 4
+	this.player.animations.add('left', [3, 4], 8, true);
 		
-		this.emitter = game.add.emitter(0, 0, 15);
+	this.emitter = game.add.emitter(0, 0, 15);
 
-		// Set the 'pixel' image for the particles
-		this.emitter.makeParticles('pixel');
+	// Set the 'pixel' image for the particles
+	this.emitter.makeParticles('pixel');
 
-		// Set the x and y speed of the particles between -150 and 150
-		// Speed will be randomly picked between -150 and 150 for each particle
-		this.emitter.setYSpeed(-150, 150);
-		this.emitter.setXSpeed(-150, 150);
+	// Set the x and y speed of the particles between -150 and 150
+	// Speed will be randomly picked between -150 and 150 for each particle
+	this.emitter.setYSpeed(-150, 150);
+	this.emitter.setXSpeed(-150, 150);
 
-		// Scale the particles from 2 time their size to 0 in 800ms
-		// Parameters are: startX, endX, startY, endY, duration
-		this.emitter.setScale(2, 0, 2, 0, 800);
+	// Scale the particles from 2 time their size to 0 in 800ms
+	// Parameters are: startX, endX, startY, endY, duration
+	this.emitter.setScale(2, 0, 2, 0, 800);
 
-		// Use no gravity
-		this.emitter.gravity = 0;
+	// Use no gravity
+	this.emitter.gravity = 0;
 		
         this.createWorld();
 
@@ -39,14 +39,14 @@ var playState = {
             { font: '18px Arial', fill: '#ffffff' });
         game.global.score = 0; 
 		
-		this.music = game.add.audio('music'); // Add the music
-		this.music.loop = true; // Make it loop
-		this.music.play(); // Start the music
-		this.sound.volume = 0.2;
+	this.music = game.add.audio('music'); // Add the music
+	this.music.loop = true; // Make it loop
+	this.music.play(); // Start the music
+	this.sound.volume = 0.2;
 		
-		this.jumpSound = game.add.audio('jump');
-		this.coinSound = game.add.audio('coin');
-		this.deadSound = game.add.audio('dead');
+	this.jumpSound = game.add.audio('jump');
+	this.coinSound = game.add.audio('coin');
+	this.deadSound = game.add.audio('dead');
 
         this.enemies = game.add.group();
         this.enemies.enableBody = true;
@@ -63,12 +63,12 @@ var playState = {
         this.movePlayer(); 
 
         if (!this.player.inWorld) {
-			// If the player is dead, do nothing
-			if (!this.player.alive) {
-				return;
-			}	
-			else	
-				this.playerDie();
+		// If the player is dead, do nothing
+		if (!this.player.alive) {
+			return;
+		}	
+		else	
+			this.playerDie();
 		}	
 		
     },
@@ -81,31 +81,31 @@ var playState = {
         if (this.cursor.left.isDown) {
         this.player.body.velocity.x = -200;
         this.player.animations.play('left'); // Left animation
-		}
-		else if (this.cursor.right.isDown) {
-			this.player.body.velocity.x = 200;
-			this.player.animations.play('right'); // Right animation
-		}
-		else {
-			this.player.body.velocity.x = 0;
-			this.player.animations.stop(); // Stop animations
-			this.player.frame = 0; // Change frame (stand still)
-		}
+	}
+	else if (this.cursor.right.isDown) {
+		this.player.body.velocity.x = 200;
+		this.player.animations.play('right'); // Right animation
+	}
+	else {
+		this.player.body.velocity.x = 0;
+		this.player.animations.stop(); // Stop animations
+		this.player.frame = 0; // Change frame (stand still)
+	}
 
         if (this.cursor.up.isDown && this.player.body.touching.down) {
-			this.jumpSound.play();
-            this.player.body.velocity.y = -320;
+		this.jumpSound.play();
+            	this.player.body.velocity.y = -320;
         }      
     },
 
     takeCoin: function(player, coin) {
         this.coinSound.play();
-		// Scale the coin to 0 to make it invisible
-		this.coin.scale.setTo(0, 0);
-		// Grow the coin back to its original scale in 300ms
-		game.add.tween(this.coin.scale).to({x: 1, y: 1}, 300).start();
-		game.add.tween(this.player.scale).to({x: 1.3, y: 1.3}, 100).yoyo(true).start();
-		game.global.score += 5;
+	// Scale the coin to 0 to make it invisible
+	this.coin.scale.setTo(0, 0);
+	// Grow the coin back to its original scale in 300ms
+	game.add.tween(this.coin.scale).to({x: 1, y: 1}, 300).start();
+	game.add.tween(this.player.scale).to({x: 1.3, y: 1.3}, 100).yoyo(true).start();
+	game.global.score += 5;
         this.scoreLabel.text = 'score: ' + game.global.score;
 
         this.updateCoinPosition();
@@ -165,21 +165,21 @@ var playState = {
     },
 
     playerDie: function() {
-		// Kill the player to make it disappear from the screen
-		this.player.kill();
-		// Flash the color white for 300ms
-		game.camera.flash(0xffffff, 300);
-		// Shake for 300ms with an intensity of 0.02
-		game.camera.shake(0.02, 300);
-		// Start the sound and the particles
-		this.deadSound.play();
-		this.emitter.x = this.player.x;
-		this.emitter.y = this.player.y;
-		this.emitter.start(true, 800, null, 15);
+	// Kill the player to make it disappear from the screen
+	this.player.kill();
+	// Flash the color white for 300ms
+	game.camera.flash(0xffffff, 300);
+	// Shake for 300ms with an intensity of 0.02
+	game.camera.shake(0.02, 300);
+	// Start the sound and the particles
+	this.deadSound.play();
+	this.emitter.x = this.player.x;
+	this.emitter.y = this.player.y;
+	this.emitter.start(true, 800, null, 15);
 		
-		this.music.stop();
+	this.music.stop();
 
-		// Call the 'startMenu' function in 1000ms
-		game.time.events.add(1000, this.startMenu, this);
+	// Call the 'startMenu' function in 1000ms
+	game.time.events.add(1000, this.startMenu, this);
     },
 };
