86cad21ddf98d27a33579f24361a8a2a867b52a1
Sam Kocher <sam.w.kocher@gmail.com>
2016-06-14 10:53:30 -0500

    fixing indentation..

--- Program-2/js/load.js
+++ Program-2/js/load.js
@@ -9,22 +9,22 @@ var loadState = {
         game.load.setPreloadSprite(progressBar);
     
         game.load.spritesheet('player', 'assets/player2.png', 20, 20);
-		game.load.spritesheet('mute', 'assets/muteButton.png', 28, 22);
-		game.load.image('pixel', 'assets/pixel.png');
+	game.load.spritesheet('mute', 'assets/muteButton.png', 28, 22);
+	game.load.image('pixel', 'assets/pixel.png');
         game.load.image('enemy', 'assets/enemy.png');
         game.load.image('coin', 'assets/money.png');
         game.load.image('wallV', 'assets/wallVertical.png');
         game.load.image('wallH', 'assets/wallHorizontal.png');
         game.load.image('background', 'assets/background.png'); 
 		
-		game.load.audio('music', ['assets/music.mp3']);
+	game.load.audio('music', ['assets/music.mp3']);
 		
-		// Sound when the player jumps
-		game.load.audio('jump', ['assets/jump.ogg', 'assets/jump.mp3']);
-		// Sound when the player takes a coin
-		game.load.audio('coin', ['assets/coin.ogg', 'assets/coin.mp3']);
-		// Sound when the player dies
-		game.load.audio('dead', ['assets/dead.ogg', 'assets/dead.mp3']);
+	// Sound when the player jumps
+	game.load.audio('jump', ['assets/jump.ogg', 'assets/jump.mp3']);
+	// Sound when the player takes a coin
+	game.load.audio('coin', ['assets/coin.ogg', 'assets/coin.mp3']);
+	// Sound when the player dies
+	game.load.audio('dead', ['assets/dead.ogg', 'assets/dead.mp3']);
     },
 
     create: function() { 
