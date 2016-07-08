3c57081fae4783831672ea0026549e19fd211eae
swkocher <sam.w.kocher@gmail.com>
2016-07-08 00:59:15 -0400

    adding program-7

---
+++ Program-7/js/game.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(500, 340);
+
+game.global = {
+    score: 0
+};
+
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('menu', menuState);
+game.state.add('play', playState);
+game.state.add('play2', play2State);
+
+game.state.start('boot');
